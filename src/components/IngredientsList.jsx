e0c926278e95711109308ee90e69935eb5a65121
Xavi <valcreativo@gmail.com>
2024-05-30 21:18:26 +0200

    ingredientsList empezado, resto de paginas creadas sin nada

---
+++ src/components/IngredientsList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function IngredientsList() {
+    const [ingredientsList, setIngredientsList]=useState()
+
+
+  return (
+    <div>IngredientsList</div>
+  )
+}
+
+export default IngredientsList
