11fe5befd9d43969368d4ffdc9d41a08bc9382e0
Xavi <valcreativo@gmail.com>
2024-05-31 17:29:22 +0200

    Ingredients en cocktailDetails

---
+++ src/components/CocktailForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CocktailForm() {
+
+
+
+    
+  return (
+    <div>CocktailForm</div>
+  )
+}
+
+export default CocktailForm
--- src/pages/CocktailDetails.jsx
+++ src/pages/CocktailDetails.jsx
@@ -1,68 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { useParams } from 'react-router-dom'
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 function CocktailDetails() {
-    const params = useParams()
+  const params = useParams();
 
-    const [cocktailDetails, setCocktailDetails]=useState(null)
+  const [cocktailDetails, setCocktailDetails] = useState(null);
 
-    useEffect(()=>{
-        axios.get(`${import.meta.env.VITE_URL_BACKEND}/cocktails/${params.cocktailId}`)
-        .then((response)=>{
-            
-            setCocktailDetails(response.data)
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
-        
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_URL_BACKEND}/cocktails/${params.cocktailId}`)
+      .then((response) => {
+        setCocktailDetails(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    },[])
-    
-    
-    
-
-    if (cocktailDetails === null){
-        return <h1>ESPERA</h1>
-    }
+  if (cocktailDetails === null) {
+    return <h1>ESPERA</h1>;
+  }
   return (
     <div>
-        <h3>{cocktailDetails.name}</h3>
-        <img src={cocktailDetails.img}/>
-        <h6>{cocktailDetails.category}</h6>
-        <h6>{cocktailDetails.description}</h6>
-        <h6>{cocktailDetails.steps}</h6>
-        {cocktailDetails.ingredients.map((eachIngredient, index)=>{
+      <h3>{cocktailDetails.name}</h3>
+      <img src={cocktailDetails.img} />
+      <h6>{cocktailDetails.category}</h6>
+      <h6>{cocktailDetails.description}</h6>
+      <h6>{cocktailDetails.steps}</h6>
+      {cocktailDetails.ingredients.map((eachIngredient, index) => {
+        return (
+          <div key={index}>
+            <h6>{`Ingredient: ${eachIngredient.name}`}</h6>
+            <h6>{`Description: ${eachIngredient.description}`}</h6>
+            <h6>{`Has alcohol: ${eachIngredient.hasAlcohol? "yes" : "no"}`}</h6>
+            <h6>{`Percentage of alcohol: ${eachIngredient.alcoholGraduation}`}</h6>
             
-            return(
-                
-                <ul key={index}>
-                    {eachIngredient.map((ingredientCategory)=>{
-                        return(
-                            <li key={ingredientCategory._id}>
-                    <h6>{ingredientCategory.name}</h6>
-                    <h6>{ingredientCategory.hasAlcohol}</h6>
-                    <h6>{ingredientCategory.alcoholGraduation}</h6>
-                    <img src={ingredientCategory.img}/>
-                    <h6>{ingredientCategory.description}</h6>
-                </li>
-
-                        )
-                    })}
-                
-                </ul>
-    
-                    
-                
-                
-                )
-        })}
-        <h6>{cocktailDetails.ingredients}</h6>
+          
+          </div>
+        );
+      })}
+      
     </div>
-  )
+  );
 }
 
-
-export default CocktailDetails
+export default CocktailDetails;
