278f9f516efd35163292acb326a3ac0f31abbac7
Xavi <valcreativo@gmail.com>
2024-06-05 03:18:52 +0200

    make cocktail and edit

--- src/pages/CocktailDetails.jsx
+++ src/pages/CocktailDetails.jsx
@@ -1,80 +1,166 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import Comment from "../components/Comment";
 import service from "../services/config.services";
-
+import { AuthContext } from "../context/auth.context";
 
 function CocktailDetails() {
   const params = useParams();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const { authenticateUser, isLoggedIn, loggedUserId } =
+    useContext(AuthContext);
 
   const [cocktailDetails, setCocktailDetails] = useState(null);
+  const [editing, setEditing] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    category: "",
+    description: "",
+    steps: "",
+    ingredients: [],
+    imageUrl: "",
+  });
 
   useEffect(() => {
     axios
-      .get(`${import.meta.env.VITE_URL_BACKEND}/api/cocktails/${params.cocktailId}`)
+      .get(
+        `${import.meta.env.VITE_URL_BACKEND}/api/cocktails/${params.cocktailId}`
+      )
       .then((response) => {
         setCocktailDetails(response.data);
+        setFormData({
+          name: response.data.name,
+          category: response.data.category,
+          description: response.description,
+          steps: response.data.steps,
+          ingredients: response.data.ingredients,
+          imageUrl: response.data.imageUrl,
+        });
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  const deleteCocktail = async () =>{
-    try {
-      const userAuthenticated = await getUserAuthenticated() //usuario identificado
-      const cocktailOwner = await getCocktailOwner(params.cocktailId)
-
-      if(userAuthenticated.id === cocktailOwner.id){
-        await await service.delete(`${import.meta.env.VITE_URL_BACKEND}/api/cocktails/${params.cocktailId}`)
-        navigate("/cocktails")
-
-      }else {
-        alert("You're not allowed to delete this cocktail")
-      }
-      
-      
-    } catch (error) {
-      
-      
-    }
-    const getUserAuthenticated = async () => {
-      //logica user auth.
-    }
-    const getCocktailOwner = async (cocktailId) => {
-      //logica obtener dueño
-    }
+  if (!cocktailDetails){
+    return <h1>ESPERA</h1>
+  }
   
-  }
+  const deleteCocktail = async () => {
+    try {
+      await authenticateUser();
+      await service.delete(
+        `${import.meta.env.VITE_URL_BACKEND}/api/cocktails/${params.cocktailId}`
+      );
+      navigate("/cocktails");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  if (cocktailDetails === null) {
-    return <h1>ESPERA</h1>;
-  }
-  return (
-    <div>
-      <button onClick={deleteCocktail}>Delete Cocktail</button>
-      <h3>{cocktailDetails.name}</h3>
-      <img src={cocktailDetails.imageUrl} />
-      <h6>{cocktailDetails.category}</h6>
-      <h6>{cocktailDetails.description}</h6>
-      <h6>{cocktailDetails.steps}</h6>
-      {cocktailDetails.ingredients.map((eachIngredient, index) => {
-        return (
-          <div key={index}>
-            <h6>{`Ingredient: ${eachIngredient.name}`}</h6>
-            <h6>{`Description: ${eachIngredient.description}`}</h6>
-            <h6>{`Has alcohol: ${eachIngredient.hasAlcohol? "yes" : "no"}`}</h6>
-            <h6>{`Percentage of alcohol: ${eachIngredient.alcoholGraduation}`}</h6>
-            
+  const handleInputChange = async (event) => {
+    const {name, value} = event.target
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value } ));
+  };
+/*   const handleEdit = () => {
+    setEditing(true);
+  }; */
+  const handleSave = async () => {
+    try {
+      await authenticateUser();
+      await service.put(
+        `${import.meta.env.VITE_URL_BACKEND}/api/cocktails/${
+          params.cocktailId}`, formData
+      );
+      setEditing(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+ 
+
+
+return (
+  <div>
+    {/* Porque funciona con ===????? preguntar a Jorge*/}
+    {cocktailDetails && isLoggedIn && loggedUserId === cocktailDetails.owner._id && (
+      <>
+        <button onClick={deleteCocktail}>Delete</button>
+        <button onClick={() => setEditing(true)}>Editar</button>
+        {editing ? (
+          <form>
+            <label>
+              Name:
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleInputChange}
+              />
+            </label>
+            <label>
+              Category:
+              <input
+                type="text"
+                name="category"
+                value={formData.category}
+                onChange={handleInputChange}
+              />
+            </label>
+            <label>
+              Description:
+              <textarea
+                name="description"
+                value={formData.description}
+                onChange={handleInputChange}
+              />
+            </label>
+            <label>
+              Steps:
+              <textarea
+                name="steps"
+                value={formData.steps}
+                onChange={handleInputChange}
+              />
+            </label>
+            <label>
+              Image:
+              <input
+                type="text"
+                name="imageUrl"
+                value={formData.imageUrl}
+                onChange={handleInputChange}
+              />
+            </label>
+            <button onClick={handleSave}>Save Changes</button>
+          </form>
+        ) : (
+          <div>
+            <h3>{cocktailDetails.name}</h3>
+            <img src={cocktailDetails.imageUrl} />
+            <h6>{cocktailDetails.category}</h6>
+            <h6>{cocktailDetails.description}</h6>
+            <h6>{cocktailDetails.steps}</h6>
+            {cocktailDetails.ingredients.map((eachIngredient, index) => {
+              return (
+                <div key={index}>
+                  <h6>{`Ingredient: ${eachIngredient.name}`}</h6>
+                  <h6>{`Description: ${eachIngredient.description}`}</h6>
+                  <h6>{`Has alcohol: ${
+                    eachIngredient.hasAlcohol ? "yes" : "no"
+                  }`}</h6>
+                  <h6>{`Percentage of alcohol: ${eachIngredient.alcoholGraduation}`}</h6>
+                </div>
+              );
+            })}
           </div>
-        );
-      })}
-      
-      {/* <Comment /> */}
-    </div>
-  );
+        )}
+      </>
+    )}
+  </div>
+);
 }
 
 export default CocktailDetails;
